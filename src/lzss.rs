2ec543406c8b21017b6b1179d7fecfef8b4a617f
Steve-xmh <stevexmh@qq.com>
2022-02-11 18:13:59 +0800

    init

---
+++ src/lzss.rs
@@ -0,0 +1,321 @@
+//! LZSS compression from https://github.com/magical/nlzss/blob/master/compress.py
+
+use std::{
+    collections::HashMap,
+    error::Error,
+    hash::Hash,
+    io::{Cursor, Write},
+    ops::Index,
+};
+
+use byteorder::*;
+use itertools::Itertools;
+
+use crate::print_data;
+
+#[derive(Debug)]
+struct DefaultMap<K, V>(HashMap<K, V>);
+
+impl<K, V> DefaultMap<K, V>
+where
+    K: Eq + Hash + Clone,
+    V: Default + Clone,
+{
+    pub fn new() -> Self {
+        Self(Default::default())
+    }
+    pub fn get_mut(&mut self, k: &K) -> &mut V {
+        if self.0.contains_key(k) {
+            self.0.get_mut(k).unwrap()
+        } else {
+            self.0.insert(k.clone(), Default::default());
+            self.0.get_mut(k).unwrap()
+        }
+    }
+    pub fn get(&mut self, k: &K) -> V {
+        if self.0.contains_key(k) {
+            self.0.get(k).cloned().unwrap()
+        } else {
+            Default::default()
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct CompressWindow<'a, const LEN: u32, const MIN: u32, const MAX: u32> {
+    pub(self) input: &'a [u8],
+    pub(self) hash: DefaultMap<u32, Vec<u32>>,
+    pub(self) full: bool,
+    pub(self) disp_min: u32,
+    pub(self) disp_start: u32,
+    pub(self) start: u32,
+    pub(self) stop: u32,
+    pub(self) index: u32,
+}
+
+pub type NLZ10Window<'a> = CompressWindow<'a, 4096, 3, { 3 + 0xF }>;
+pub type NLZ11Window<'a> = CompressWindow<'a, 4096, 3, { 0x111 + 0xFFFF }>;
+
+impl<'a, const LEN: u32, const MIN: u32, const MAX: u32> CompressWindow<'a, LEN, MIN, MAX> {
+    fn new(input: &'a [u8]) -> Self {
+        Self {
+            input,
+            hash: DefaultMap::new(),
+            full: false,
+            disp_min: 2,
+            disp_start: 1,
+            start: 0,
+            stop: 0,
+            index: 0,
+        }
+    }
+
+    fn input_len(&self) -> u32 {
+        self.input.len() as _
+    }
+
+    fn next(&mut self) {
+        if self.index < self.disp_start - 1 {
+            self.index += 1;
+            return;
+        }
+        if self.full {
+            let olditem = self.input[self.start as usize] as _;
+            debug_assert!(self.hash.get(&olditem).get(0) == Some(&self.start));
+            self.hash.get_mut(&olditem).remove(0);
+        }
+        let item = self.input[self.stop as usize] as u32;
+        self.hash.get_mut(&item).push(self.stop);
+        self.stop += 1;
+        self.index += 1;
+
+        if self.full {
+            self.start += 1;
+        } else if LEN <= self.stop {
+            self.full = true;
+        }
+    }
+
+    fn advance(&mut self, n: u32) {
+        for _ in 0..n {
+            self.next();
+        }
+    }
+
+    fn search(&mut self) -> Option<(u32, i32)> {
+        let mut counts = vec![];
+        let input_byte = self.input[self.index as usize] as _;
+        let indices = self.hash.get(&input_byte);
+        for i in indices.iter() {
+            let matchlen = self.match_data(*i, self.index);
+            if matchlen >= MIN {
+                let disp = self.index as i32 - *i as i32;
+                // debug_assert!(self.index as i32 - disp >= 0);
+                // debug_assert!(self.disp_min as i32 <= disp);
+                // debug_assert!(disp <= (LEN + self.disp_min) as i32);
+                if self.disp_min as i32 <= disp {
+                    counts.push((matchlen, -disp));
+                    if matchlen >= MAX {
+                        return counts.last().cloned();
+                    }
+                }
+            }
+        }
+        if !counts.is_empty() {
+            let mut result = (0, 0);
+            let mut t = 0;
+            for count in counts {
+                if t < count.0 {
+                    t = count.0;
+                    result = count;
+                }
+            }
+            return Some(result);
+        }
+        None
+    }
+
+    fn match_data(&self, start: u32, bufstart: u32) -> u32 {
+        let size = self.index - start;
+        if size == 0 {
+            return 0;
+        }
+        let mut matchlen = 0;
+        let it = 0..(self.input.len() as u32 - bufstart).min(MAX);
+        for i in it {
+            if self.input[(start + (i % LEN)) as usize] == self.input[(bufstart + i) as usize] {
+                matchlen += 1;
+            } else {
+                break;
+            }
+        }
+        matchlen
+    }
+}
+
+impl<const LEN: u32, const MIN: u32, const MAX: u32> Index<usize>
+    for CompressWindow<'_, LEN, MIN, MAX>
+{
+    type Output = u8;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.input[index]
+    }
+}
+
+#[derive(Debug)]
+struct Compressor<'a, const LEN: u32, const MIN: u32, const MAX: u32>(
+    CompressWindow<'a, LEN, MIN, MAX>,
+    u32,
+);
+
+impl<'a, const LEN: u32, const MIN: u32, const MAX: u32> Compressor<'a, LEN, MIN, MAX> {
+    pub fn new(window: CompressWindow<'a, LEN, MIN, MAX>) -> Self {
+        Self(window, 0)
+    }
+}
+
+#[derive(Debug)]
+enum CompressChunkType {
+    Replace(u32, i32),
+    Data(u8),
+}
+
+impl<const LEN: u32, const MIN: u32, const MAX: u32> Iterator for Compressor<'_, LEN, MIN, MAX> {
+    type Item = CompressChunkType;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.1 >= self.0.input_len() {
+            return None;
+        }
+        if let Some(matched) = self.0.search() {
+            self.0.advance(matched.0);
+            self.1 += matched.0;
+            // assert_ne!(matched.1, -283);
+            Some(CompressChunkType::Replace(matched.0, matched.1))
+        } else {
+            let i = self.1;
+            self.0.next();
+            self.1 += 1;
+            Some(CompressChunkType::Data(self.0[i as _]))
+        }
+    }
+}
+
+pub fn compress_nlz10(input: &[u8], output: &mut impl Write) -> Result<(), Box<dyn Error>> {
+    output.write_u32::<LE>(((input.len() as u32) << 8) + 0x10)?;
+    let mut length = 0;
+    let window = NLZ10Window::new(input);
+    for c in &Compressor::new(window).chunks(8) {
+        let c = c.collect_vec();
+        let mut flag = 0u8;
+        let mut flagit = c
+            .iter()
+            .map(|x| matches!(x, CompressChunkType::Replace(_, _)));
+        for _ in 0..8 {
+            flag <<= 1;
+            if let Some(c) = flagit.next() {
+                if c {
+                    flag |= 1;
+                }
+            }
+        }
+        output.write_u8(flag)?;
+        length += 1;
+
+        for c in c {
+            match c {
+                CompressChunkType::Replace(mut count, disp) => {
+                    count -= 3;
+                    let disp = (-disp) - 1;
+                    assert!(0 <= disp);
+                    assert!(disp < 4096);
+                    let sh = (((count << 12) | disp.abs() as u32) & 0xFFFF) as u16;
+                    output.write_u16::<BE>(sh)?;
+                    length += 2;
+                    
+                }
+                CompressChunkType::Data(data) => {
+                    output.write_u8(data)?;
+                    length += 1;
+                }
+            }
+        }
+    }
+    let padding = if length % 4 == 0 { 0 } else { 4 - length % 4 };
+    for _ in 0..padding {
+        output.write_u8(0xFF)?;
+    }
+
+    Ok(())
+}
+pub fn compress_nlz11(input: &[u8], output: &mut impl Write) -> Result<(), Box<dyn Error>> {
+    output.write_u32::<LE>(((input.len() as u32) << 8) + 0x11)?;
+    let mut length = 0;
+
+    let window = NLZ11Window::new(input);
+    for c in &Compressor::new(window).chunks(8) {
+        let c = c.collect_vec();
+        let mut flag = 0u8;
+        let mut flagit = c
+            .iter()
+            .map(|x| matches!(x, CompressChunkType::Replace(_, _)));
+        for _ in 0..8 {
+            flag <<= 1;
+            if let Some(c) = flagit.next() {
+                if c {
+                    flag |= 1;
+                }
+            }
+        }
+        output.write_u8(flag)?;
+        length += 1;
+        for c in c {
+            match c {
+                CompressChunkType::Replace(mut count, disp) => {
+                    let disp = (-disp) - 1;
+                    debug_assert!((0..=0xFFF).contains(&disp));
+                    if count <= 1 + 0xF {
+                        count -= 1;
+                        debug_assert!((2..=0xF).contains(&count));
+                        let sh = ((count << 12) & 0xFFFF) as u16 | (disp.abs() & 0xFFFF) as u16;
+                        output.write_u16::<BE>(sh)?;
+                        length += 2;
+                    } else if count <= 0x11 + 0xFF {
+                        count -= 0x11;
+                        debug_assert!((0..=0xFF).contains(&count));
+                        let b = (count >> 4 & 0xFF) as u8;
+                        let sh = ((count & 0xF) << 12) as u16 | disp.abs() as u16;
+                        output.write_u8(b)?;
+                        output.write_u16::<BE>(sh)?;
+                        length += 3;
+                    } else if count <= 0x111 + 0xFFFF {
+                        count -= 0x111;
+                        debug_assert!((0..=0xFFFF).contains(&count));
+                        let l = (1 << 28) | (count << 12) | disp.abs() as u32;
+                        output.write_u32::<BE>(l as _)?;
+                        length += 4;
+                    } else {
+                        panic!();
+                    }
+                }
+                CompressChunkType::Data(data) => {
+                    output.write_u8(data)?;
+                    length += 1;
+                }
+            }
+        }
+    }
+    let padding = if length % 4 == 0 { 0 } else { 4 - length % 4 };
+    for _ in 0..padding {
+        output.write_u8(0xFF)?;
+    }
+
+    Ok(())
+}
+
+pub fn compress_arr(input: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
+    let mut result = Cursor::new(Vec::new());
+    compress_nlz10(input, &mut result)?;
+    Ok(result.into_inner())
+}
---
+++ src/main.rs
@@ -0,0 +1,269 @@
+mod lzss;
+
+use std::{
+    fs::OpenOptions,
+    io::{Read, Seek, SeekFrom, Write},
+};
+
+use byteorder::*;
+
+#[derive(Debug)]
+struct SubFile {
+    offset: u32,
+    size: u32,
+    compressed: bool,
+}
+
+fn print_data(data: &[u8]) {
+    for b in data {
+        print!("{:02X}", b);
+    }
+    println!();
+}
+
+fn main() {
+    let args = zigarg::Arguments::new();
+    println!("Star Force Archive Tool (Rust) v1.0 by SteveXMH (Original by Prof.9)");
+    if !args.has_args() {
+        println!();
+        println!("Usage:  sfarctool.exe <options>");
+        println!("Options:");
+        println!("        -i [path]       Specifies input path.");
+        println!("        -o [path]       Specifies output path.");
+        println!("        -x              Unpacks archive to folder. Requires -i and -o.");
+        println!("        -p              Packs folder to archive. Requires -i and -o.");
+        println!("        -eof            Indicates the archive has an EOF subfile entry. Requires -x or -p.");
+        println!("        -c              Compress sub files if can be smaller. Requires -p.");
+        println!("        -v              Toggle verbose mode which will output a lot of message.");
+        println!();
+        println!("For option -p, subfiles in the input directory must be named as \"XXX.ext\" or \"name_XXX.ext\", where \"name\" is an arbitrary string not containing '.' or '_', \"XXX\" is the subfile number and \"ext\" is any extension (multiple extensions are allowed. Any files that do not adhere to this format will be skipped.");
+        return;
+    }
+    let input = args
+        .get_value("-i")
+        .cloned()
+        .expect("Not selected an input path");
+    let output = args
+        .get_value("-o")
+        .cloned()
+        .expect("Not selected a output path");
+    let unpack = args.exist("-x");
+    let pack = args.exist("-p");
+    let eof = args.exist("-eof");
+    let compress = args.exist("-c");
+    let verbose = args.exist("-v");
+    if (unpack && pack) || (!unpack && !pack) {
+        println!("Error: Both set pack and unpack mode.");
+        return;
+    }
+    if unpack {
+        if verbose {
+            println!("Unpacking archive");
+        }
+        let mut subfiles: Vec<SubFile> = Vec::new();
+        let mut file = OpenOptions::new()
+            .read(true)
+            .open(&input)
+            .expect("Can't open input archive");
+        let file_size = file
+            .metadata()
+            .expect("Can't get metadata of the input archive")
+            .len() as usize;
+        let mut header_end = file_size;
+        let mut pos = 0;
+        let mut max_size = 0;
+        while pos < header_end {
+            let offset = file.read_u32::<LE>().expect("Can't read offset");
+            let size = file.read_u32::<LE>().expect("Can't read size");
+            pos += 8;
+            let subfile = SubFile {
+                offset: offset as _,
+                size: (size & 0x7FFFFFFF) as _,
+                compressed: (size & 0x80000000) != 0,
+            };
+            if verbose {
+                if subfile.compressed {
+                    println!(
+                        "Entry {} at 0x{:08x}, size 0x{:08x}, compressed",
+                        subfiles.len(),
+                        subfile.offset,
+                        subfile.size
+                    );
+                } else {
+                    println!(
+                        "Entry {} at 0x{:08x}, size 0x{:08x}",
+                        subfiles.len(),
+                        subfile.offset,
+                        subfile.size
+                    );
+                }
+            }
+            subfiles.push(subfile);
+            header_end = (offset as usize).min(header_end);
+            max_size = (size as usize).max(max_size);
+        }
+        if pos != header_end
+            || (eof
+                && (subfiles.last().expect("").size > 0 || subfiles.last().expect("").compressed))
+        {
+            println!("Invalid archive file header.");
+            return;
+        }
+        if eof {
+            subfiles.pop();
+        }
+        let subfile_len = subfiles.len() - 1;
+        let output_basename = {
+            if let Some(sep) = input.as_str().rfind(['/', '\\']) {
+                if let Some(dot) = input.as_str().rfind('.') {
+                    input.as_str()[sep + 1..dot].to_owned()
+                } else {
+                    input.as_str()[sep + 1..].to_owned()
+                }
+            } else {
+                input.to_owned()
+            }
+        };
+        let max_size = subfiles.iter().map(|x| x.size).max().unwrap_or_default();
+        if verbose {
+            println!("Largest entry size: 0x{:08x}", max_size);
+        }
+        let mut buf = vec![0; max_size as _];
+        let padding = (subfiles.len() - 1).to_string().len();
+        let to_padded_string = |num: u32| -> String {
+            let num = num.to_string();
+            let mut padding = "0".to_string().repeat(padding - num.len());
+            padding.push_str(num.as_str());
+            padding
+        };
+        let output_basename = output_basename;
+        std::fs::create_dir_all(&output).expect("Can't create output directory");
+        for (i, subfile) in subfiles.into_iter().enumerate() {
+            let subfile = subfile;
+            if i == subfile_len
+                && subfile.offset == file_size as _
+                && subfile.size == 0xFFFF
+                && !subfile.compressed
+            {
+                continue;
+            }
+            let output_name = format!("{}_{}.bin", output_basename, to_padded_string(i as _));
+            let mut output = std::path::PathBuf::from(&output);
+            output.push(output_name);
+            let mut output = std::fs::OpenOptions::new()
+                .write(true)
+                .truncate(true)
+                .create(true)
+                .open(output)
+                .expect("Can't write subfile");
+            file.seek(SeekFrom::Start(subfile.offset as _))
+                .expect("Can't seek file");
+            if subfile.compressed {
+                if verbose {
+                    println!("Decompressing entry {}", i);
+                }
+                let decompressed =
+                    nintendo_lz::decompress(&mut file).expect("Can't decompress file");
+                output
+                    .write_all(&decompressed)
+                    .expect("Can't write subfile");
+            } else {
+                if verbose {
+                    println!("Unpacking entry {}", i);
+                }
+                file.read_exact(&mut buf[..subfile.size as usize])
+                    .expect("Can't read file");
+                output
+                    .write_all(&buf[..subfile.size as usize])
+                    .expect("Can't write subfile");
+            }
+        }
+    } else if pack {
+        // Parse files and sort them.
+        let mut input_dir = std::fs::read_dir(&input).expect("Can't read input directory");
+        let mut files = Vec::new();
+        while let Some(Ok(entry)) = input_dir.next() {
+            if entry.path().is_file() {
+                let file_name = entry.file_name();
+                let file_name = file_name.to_string_lossy();
+                let dot = file_name.rfind('.');
+                let underline = file_name.rfind('_');
+                if let (Some(dot), Some(underline)) = (dot, underline) {
+                    if let Ok(index) = file_name[underline + 1..dot].parse::<usize>() {
+                        files.push((index, entry.path()))
+                    }
+                }
+            }
+        }
+        files.sort_by(|a, b| a.0.cmp(&b.0));
+        let header_size = if eof {
+            (files.len() + 2) * 8
+        } else {
+            (files.len() + 1) * 8
+        };
+        let mut offset = header_size as u32;
+        let mut file = OpenOptions::new()
+            .write(true)
+            .truncate(true)
+            .create(true)
+            .open(&output)
+            .expect("Can't open output file");
+        let mut file_size = header_size;
+        for _ in 0..files.len() + 1 {
+            file.write_u64::<LE>(0).expect("Can't fill header");
+        }
+        let mut buf = Vec::new();
+        let files = files
+            .into_iter()
+            .map(|(i, entry)| {
+                let mut subfile = OpenOptions::new()
+                    .read(true)
+                    .open(&entry)
+                    .expect("Can't open subfile");
+                buf.clear();
+                subfile.read_to_end(&mut buf).expect("Can't read subfile");
+                let uncompressed_size = buf.len();
+                if compress {
+                    if let Ok(compressed_data) = lzss::compress_arr(&buf) {
+                        if uncompressed_size <= compressed_data.len() {
+                            file_size += uncompressed_size;
+                            (false, buf.to_owned(), uncompressed_size)
+                        } else {
+                            file_size += compressed_data.len();
+                            (true, compressed_data, uncompressed_size)
+                        }
+                    } else {
+                        (false, buf.to_owned(), uncompressed_size)
+                    }
+                } else {
+                    (false, buf.to_owned(), uncompressed_size)
+                }
+            })
+            .collect::<Vec<_>>();
+        file.seek(SeekFrom::Start(0))
+            .expect("Can't seek file to start");
+        for (compressed, data, uncompressed_size) in &files {
+            file.write_u32::<LE>(offset)
+                .expect("Can't write file size at end of entries");
+            file.write_u32::<LE>(
+                (*uncompressed_size as u32 & 0x7FFFFFFF) | if *compressed { 0x80000000 } else { 0 },
+            )
+            .expect("Can't write file size at end of entries");
+            offset += data.len() as u32;
+        }
+        if eof {
+            file.write_u32::<LE>(file_size as _)
+                .expect("Can't write file size at end of entries");
+            file.write_u32::<LE>(0)
+                .expect("Can't write file size at end of entries");
+        }
+        file.write_u32::<LE>(file_size as _)
+            .expect("Can't write file size at end of entries");
+        file.write_u32::<LE>(0xFFFF)
+            .expect("Can't write file size at end of entries");
+        for (_, data, _) in files {
+            file.write_all(&data)
+                .expect("Can't write subfile to archive")
+        }
+    }
+}
